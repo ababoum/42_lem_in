eb2538f74cba7f6f7ab2aff118bead34b4097c47
Marwa <marwa@meetdeal.fr>
2023-03-08 13:11:18 +0100

    [FIX] Bugfix

--- libft/src/string/ft_strlen.c
+++ libft/src/string/ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: marwa <marwa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 12:52:04 by mdaoud            #+#    #+#             */
-/*   Updated: 2018/11/23 18:05:41 by marwa            ###   ########.fr       */
+/*   Updated: 2023/03/08 13:10:43 by marwa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlen(char *s)
+size_t		ft_strlen(const char *s)
 {
 	int i;
 
